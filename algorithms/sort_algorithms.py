1daba1fb1809b1841e4eb28e14455cceb964b9e6
Youness <youness.aliyari@gmail.com>
2024-04-08 00:13:36 -0400

    Initial commit Playing with GUI, trying to select the sorting algo and run it. Todat only Insertion sort

---
+++ algorithms/sort_algorithms.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import os
+""" A simple implementation of the Insertion sort"""
+
+
+def insertion_sort(array):
+    n = len(array)
+    if n <= 1:
+        return array  # array of length 0 or 1 is already sorted
+
+    for i in range(0, n):
+        key = array[i]
+        j = i-1
+        while j >= 0 and array[j] > key:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+    return array
+
+
+def bubble_sort(array):
+    return array
+
+
+def merge_sort(array):
+    return array
---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+from algorithms import sort_algorithms
+from gui import gui_func
+def main():
+    gui_func
+
+if __name__ == "__main__":
+    main()
